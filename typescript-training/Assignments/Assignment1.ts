7b9958b10b80908bcf1dc0684d27585156fcfa9b
Mahesh Pasumarthy <pasumarthymahesh8@gmail.com>
2026-07-30 12:31:37 +0530

    Assignment1

---
+++ typescript-training/Assignments/Assignment1.ts
@@ -0,0 +1,65 @@
+//Temperature of a city in degrees Celsius: 25.5
+let temperature: number=25.5;
+console.log(`Temperature of a city in degrees Celsius:${temperature}`);
+//Whether a customer has placed an order: true or false
+//Person's phone number: "123-456-7890"
+let phoneNumber:string = "123-456-7890";
+console.log(`Person's phone number: ${phoneNumber}`);
+//Amount of money in a customer's bank account: 1000.50
+let money:number = 1000.50;
+console.log(`Amount of money in a customer's bank account: ${money}`);
+//Person's email address: "john.doe@example.com"
+let email:string = "john.doe@example.com";
+console.log(`Person's email address: ${email}`);
+//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+interface locationDetails{
+    latitude:number,
+    longitude:number
+
+}
+let locationInfo:locationDetails={
+    latitude: 37.7749,
+    longitude: -122.4194
+};
+console.log("Location Details :",locationInfo.latitude);
+console.log("Location Details :",locationInfo.longitude);
+//Person's marital status: true or false
+//Person's occupation: "Software Engineer"
+let personsOccupation:string = "Software Engineer";
+console.log(`Person's occupation: ${personsOccupation}`);
+//Person's favourite colour: "Blue"
+let favouriteColour:string = "Blue";
+console.log(`Person's favourite colour: ${favouriteColour}`);
+//10.Current year: 2023
+let currentYear:number =2023;
+console.log(`Current year: ${currentYear}`);
+//11.Number of followers on a social media platform: 1,000,000
+let numberOfFollowers:number = 1000000;
+console.log(`Number of followers on a social media platform: ${numberOfFollowers}`);
+//12.Rating of a movie: 7.5
+let ratingOfMovie:number = 7.5;
+console.log(`Rating of a movie: ${ratingOfMovie}`);
+//13.Person's blood type: 'A'
+let personsBloodType:string ="A";
+console.log(`Person's blood type: ${personsBloodType}`);
+//14.Title of a book: "To Kill a Mockingbird"
+let titleOfBook:string = "To Kill a Mockingbird";
+console.log(`Title of a book: ${titleOfBook}`);
+//15.Number of employees in a company: 500
+let numberOfEmployees:number = 500;
+console.log(`Number of employees in a company: ${numberOfEmployees}`);
+//16.Time of an event: 2:30 PM
+let timeOfEvent:string = "2:30 PM";
+console.log(`Time of an event: ${timeOfEvent}`);
+//17.Name of a country: "United States"
+let countryname:string = "United States";
+console.log(`Name of a country: ${countryname}`);
+//18.Person's eye color: "Brown"
+let personsEyeColor:string ="Brown";
+console.log(`Person's eye color: ${personsEyeColor}`);
+//19.Person's birthplace: "New York City"
+let personsBirthplace:string ="New York City";
+console.log(`Person's birthplace: ${personsBirthplace}`);
+//20. Distance between two cities: 200.5
+let distane:number = 200.5;
+console.log(`Distance between two cities: ${distane}`);
